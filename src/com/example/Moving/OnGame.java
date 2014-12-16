a81e41d213e8a233536a819cb7889235b3e0a162
Tiny <zjutiny@gmail.com>
2014-12-17 01:09:20 +0800

    Add BGM to the first activity.

--- src/com/example/Moving/OnGame.java
+++ src/com/example/Moving/OnGame.java
@@ -2,17 +2,25 @@ package com.example.Moving;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 
+import java.io.IOException;
+
 public class OnGame extends Activity {
     /**
      * Called when the activity is first created.
      */
+    final static String LOG_TAG="StartApp";
+    MediaPlayer mp;
+    AudioManager am;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +28,24 @@ public class OnGame extends Activity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(view);
+
+        //Play the BGM when first activity starts
+        mp=new MediaPlayer();
+        //am=(AudioManager) this.getSystemService(this.AUDIO_SERVICE);
+        try {
+            //TODO: Change the source
+            mp.setDataSource("/storage/emulated/0/Download/secret-base.mp3");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            mp.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mp.start();
+
+        //Edit the animation when the game starts
         AlphaAnimation aa = new AlphaAnimation(0.3f,1.0f);
         aa.setDuration(2000);
         view.startAnimation(aa);
@@ -31,6 +57,7 @@ public class OnGame extends Activity {
                     @Override
                     public void onClick(View v) {
                         redirectTo();
+                        mp.stop();
                     }
                 });
             }
@@ -41,8 +68,29 @@ public class OnGame extends Activity {
         });
     }
     private void redirectTo(){
+        Log.d(LOG_TAG,"Game:Direct to the next activity");
         Intent intent = new Intent(this, Main.class);
         startActivity(intent);
         finish();
+    }
+
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        Log.d(LOG_TAG,"Game:onBackPressed");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        mp.start();
+        Log.d(LOG_TAG,"Game:Resume");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        mp.pause();
+        Log.d(LOG_TAG,"Game:Pause");
     }
 }
