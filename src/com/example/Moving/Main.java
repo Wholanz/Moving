c6eb08e84483f5bee19673688b4f763720ff02ab
Tiny <zjutiny@gmail.com>
2014-12-16 23:47:03 +0800

    hide bars

--- src/com/example/Moving/Main.java
+++ src/com/example/Moving/Main.java
@@ -2,6 +2,8 @@ package com.example.Moving;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by tiny on 12/16/14.
@@ -10,6 +12,8 @@ public class Main extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.start);
     }
 }
