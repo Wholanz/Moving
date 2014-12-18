f68175bac700a2540d9fc4dd4227348a3301adf0
Tiny <zjutiny@gmail.com>
2014-12-19 00:09:51 +0800

    Update MainList

--- src/com/example/Moving/ItemAdapter.java
+++ src/com/example/Moving/ItemAdapter.java
@@ -24,13 +24,27 @@ public class ItemAdapter extends ArrayAdapter<MainListItem>{
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
         MainListItem listItem=getItem(position);
-        View view= LayoutInflater.from(getContext()).inflate(resourceId, null);
-        ImageView itemImage=(ImageView)view.findViewById(R.id.item_image);
-        ImageView itemTo=(ImageView)view.findViewById(R.id.item_to);
-        TextView itemName=(TextView)view.findViewById(R.id.item_name);
-        itemImage.setImageResource(listItem.getItemImageId());
-        itemTo.setImageResource(listItem.getItemToId());
-        itemName.setText(listItem.getName());
+        View view;
+        ViewHolder viewHolder;
+        if(convertView==null) {
+            view= LayoutInflater.from(getContext()).inflate(resourceId, null);
+            viewHolder=new ViewHolder();
+            viewHolder.itemImage=(ImageView)view.findViewById(R.id.item_image);
+            viewHolder.itemTo=(ImageView)view.findViewById(R.id.item_to);
+            viewHolder.itemName=(TextView)view.findViewById(R.id.item_name);
+            view.setTag(viewHolder);
+        } else{
+            view=convertView;
+            viewHolder=(ViewHolder)view.getTag();
+        }
+        viewHolder.itemImage.setImageResource(listItem.getItemImageId());
+        viewHolder.itemTo.setImageResource(listItem.getItemToId());
+        viewHolder.itemName.setText(listItem.getName());
         return view;
+    }
+    class ViewHolder{
+        ImageView itemImage=null;
+        ImageView itemTo=null;
+        TextView itemName=null;
     }
 }
